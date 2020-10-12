5c5c7da6df446f6c72582b1425b195b5e520d3f6
Frederic DUR <fdur@sierrawireless.com>
2020-10-12 16:19:57 +0200

    AVC bootstrap rollback improvements
    Improve bootstrap rollback mechanism to manage more error cases.
    Because of this improvement, fix for LE-15324 can be reverted
    
    Resolves: LE-15536
    Change-Id: I76b94a5fb387e8c5166d97ecdf4b45ce674e3a03
    Relates: LE-15401

--- components/le_pa_secStoreTz/pa_secStoreTz.c
+++ components/le_pa_secStoreTz/pa_secStoreTz.c
@@ -1422,7 +1422,7 @@ le_result_t pa_secStore_Read
         size_t bufSizeBackup = *bufSizePtr;
         result = Read(appName, pathPtr, bufPtr, bufSizePtr);
 
-        if ((result == LE_OK) && ((*bufSizePtr) > 0))
+        if (result == LE_OK)
         {
             return result;
         }
@@ -1430,10 +1430,6 @@ le_result_t pa_secStore_Read
         {
             // Unsuccessful TrustZone read may have changed the output buffer size, so it needs
             // to be restored to its original value
-            if(!(*bufSizePtr))
-            {
-                LE_WARN("Credential len from TZ = 0");
-            }
             *bufSizePtr = bufSizeBackup;
             result = ModemRead(pathPtr, bufPtr, bufSizePtr);
 
